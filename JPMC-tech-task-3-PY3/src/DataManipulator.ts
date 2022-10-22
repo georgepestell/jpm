36c2fa7ac6fd3ddb6f665f4d28962bee4e27e1ab
ghtpestell@protonmail.com <ghtpestell@protonmail.com>
2022-10-22 13:37:09 +0100

    cleanup node_modules

--- JPMC-tech-task-3-PY3/src/DataManipulator.ts
+++ JPMC-tech-task-3-PY3/src/DataManipulator.ts
@@ -4,10 +4,10 @@ export interface Row {
   price_abc: number,
   price_def: number,
   ratio: number,
+  timestamp: Date,
   upper_bound: number,
   lower_bound: number,
   trigger_alert: number | undefined,
-  timestamp: Date,
 }
 
 
@@ -16,8 +16,8 @@ export class DataManipulator {
     const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = 1.10;
-    const lowerBound = 0.90;
+    const upperBound = 1 + 0.10;
+    const lowerBound = 1 - 0.10;
     return {
       price_abc: priceABC,
       price_def: priceDEF,
